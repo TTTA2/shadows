c7312db882e2d0e97789dbd0d8b4b10b88ed4d7a
ad <nanasisine@gmail.com>
2024-05-30 08:23:02 +0900

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+  import { Button } from "$lib/components/ui/button";
   import { invoke } from "@tauri-apps/api/tauri";
-
+  import * as Tabs from "$lib/components/ui/tabs/index";
+  import * as Card from "$lib/components/ui/card/index";
+    import { Label } from "$lib/components/ui/label";
+    import { Input } from "$lib/components/ui/input";
+  
   let name = "";
   let greetMsg = "";
 
@@ -12,6 +17,62 @@
 
 <div class="container">
   <h1>Welcome to Tauri!</h1>
+
+  <Button>aa</Button>
+
+   
+  <Tabs.Root value="account" class="w-[400px]">
+    <Tabs.List class="grid w-full grid-cols-2">
+      <Tabs.Trigger value="account">Account</Tabs.Trigger>
+      <Tabs.Trigger value="password">Password</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="account">
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Account</Card.Title>
+          <Card.Description>
+            Make changes to your account here. Click save when you're done.
+          </Card.Description>
+        </Card.Header>
+        <Card.Content class="space-y-2">
+          <div class="space-y-1">
+            <Label for="name">Name</Label>
+            <Input id="name" value="Pedro Duarte" />
+          </div>
+          <div class="space-y-1">
+            <Label for="username">Username</Label>
+            <Input id="username" value="@peduarte" />
+          </div>
+        </Card.Content>
+        <Card.Footer>
+          <Button>Save changes</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Tabs.Content>
+    <Tabs.Content value="password">
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Password</Card.Title>
+          <Card.Description>
+            Change your password here. After saving, you'll be logged out.
+          </Card.Description>
+        </Card.Header>
+        <Card.Content class="space-y-2">
+          <div class="space-y-1">
+            <Label for="current">Current password</Label>
+            <Input id="current" type="password" />
+          </div>
+          <div class="space-y-1">
+            <Label for="new">New password</Label>
+            <Input id="new" type="password" />
+          </div>
+        </Card.Content>
+        <Card.Footer>
+          <Button>Save password</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Tabs.Content>
+  </Tabs.Root>
 
   <div class="row">
     <a href="https://vitejs.dev" target="_blank">
